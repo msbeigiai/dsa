bfe6360ff6399f85fbdbc241bb31ea5a8647eb36
msbeigiai <msbeigi83@gmail.com>
2023-11-25 22:24:33 +0200

    Refactor project

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	/*int A[] = { 2, 4, 1, 3, 5, 11 };
+	
+	cout << sizeof(A) << endl;
+	cout << A[1] << endl;
+
+	printf("%d\n", A[2]);
+
+	for (int x : A) {
+		cout << x << endl;
+	}*/
+
+	int n;
+	cout << "Enter size: ";
+	cin >> n;
+	int A[n];
+	A[0] = 1;
+
+	return 0;
+}
